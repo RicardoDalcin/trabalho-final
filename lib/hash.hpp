ac667100340d13bc0c908b25139bda25e0564721
Ricardo <ricardo.dalcinh@gmail.com>
2021-11-18 21:01:59 -0300

    :sparkles: Hash table funcionando apenas com struct

---
+++ lib/hash.hpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+#include <fstream>
+
+#ifndef hash_h
+#define hash_h
+
+using namespace std;
+
+template <typename T, typename TKey>
+struct entry
+{
+  TKey id;
+  T value;
+  struct entry<T, TKey> *nextEntry;
+};
+
+template <typename T, typename TKey>
+class HashTable
+{
+private:
+  int size;
+  entry<T, TKey> *createEntry(TKey key, T *values);
+  entry<T, TKey> **hashTable;
+
+public:
+  int hashFunction(TKey key);
+  void insertItem(TKey key, T *values);
+  T searchItem(TKey key);
+  HashTable(int tableSize);
+};
+
+template <typename T, typename TKey>
+HashTable<T, TKey>::HashTable(int tableSize)
+{
+  size = tableSize;
+  hashTable = (entry<T, TKey> **)calloc(size, sizeof(entry<T, TKey> *));
+}
+
+template <typename T, typename TKey>
+entry<T, TKey> *HashTable<T, TKey>::createEntry(TKey key, T *values)
+{
+  entry<T, TKey> *newNode = new entry<T, TKey>;
+
+  newNode->id = key;
+  newNode->nextEntry = NULL;
+
+  memcpy(&newNode->value, values, sizeof(T));
+
+  return newNode;
+}
+
+template <typename T, typename TKey>
+int HashTable<T, TKey>::hashFunction(TKey key)
+{
+  unsigned long longHash = 5381;
+  int hash;
+
+  for (char &currentChar : key)
+  {
+    longHash = ((longHash << 5) + longHash) + currentChar;
+  }
+
+  hash = longHash % size;
+
+  return hash;
+}
+
+template <typename T, typename TKey>
+void HashTable<T, TKey>::insertItem(TKey key, T *values)
+{
+  int hashKey = hashFunction(key);
+
+  entry<T, TKey> *node = hashTable[hashKey];
+
+  if (node == NULL)
+  {
+    hashTable[hashKey] = createEntry(key, values);
+    return;
+  }
+
+  entry<T, TKey> *next;
+
+  while (node != NULL)
+  {
+    next = node;
+    node = next->nextEntry;
+  }
+
+  next->nextEntry = createEntry(key, values);
+}
+
+template <typename T, typename TKey>
+T HashTable<T, TKey>::searchItem(TKey key)
+{
+  int hashKey = hashFunction(key);
+
+  entry<T, TKey> *node = hashTable[hashKey];
+
+  // if (node == NULL)
+  //   return NULL;
+
+  while (node->id != key)
+  {
+    node = node->nextEntry;
+
+    // if (node == NULL)
+    // {
+    //   default(T);
+    // }
+  }
+
+  return node->value;
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
+// Standard libraries
 #include <bits/stdc++.h>
 #include <iostream>
 
+// External libraries
 #include "../include/parser.hpp"
+
+// Project libraries
+#include "../lib/hash.hpp"
 
 using namespace std;
 using namespace aria::csv;
@@ -13,18 +18,48 @@ string PLAYERS_DATASET_PATH = DATASETS_FOLDER_PATH + "/players.csv";
 string RATING_DATASET_PATH = DATASETS_FOLDER_PATH + "/rating.csv";
 string TAGS_DATASET_PATH = DATASETS_FOLDER_PATH + "/tags.csv";
 
+typedef struct teste
+{
+  int id;
+  string name;
+  int age;
+} teste;
+
 int main()
 {
-  ifstream miniratingFile(MINIRATING_DATASET_PATH);
+  // ifstream miniratingFile(MINIRATING_DATASET_PATH);
 
-  CsvParser parser(miniratingFile);
+  // CsvParser parser(miniratingFile);
 
-  for (auto &row : parser)
-  {
-    for (auto &field : row)
-    {
-      cout << field << " | ";
-    }
-    cout << endl;
-  }
+  // for (auto &row : parser)
+  // {
+  //   for (auto &field : row)
+  //   {
+  //     cout << field << " | ";
+  //   }
+  //   cout << endl;
+  // }
+
+  HashTable<teste, string> hashTable(2000);
+
+  teste *player = new teste;
+  teste *playerReturn = new teste;
+
+  player->age = 19;
+  player->name = "Ricardo Dalcin";
+  player->id = 12345;
+
+  hashTable.insertItem("Teste", player);
+
+  *playerReturn = hashTable.searchItem("Teste");
+
+  cout << playerReturn->name << playerReturn->age << playerReturn->id;
+
+  // HashTable<int, string> intHashTable(2000);
+
+  // int intTeste = 50;
+
+  // intHashTable.insertItem("Chave do Teste", (int *)intTeste);
+
+  // cout << intHashTable.searchItem("Chave do Teste");
 }
