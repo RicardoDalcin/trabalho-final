abefec9ddff812e0072761b461dd8e5e1a60fc9c
Ricardo <ricardo.dalcinh@gmail.com>
2021-11-21 14:55:18 -0300

    ♻️ Isola mensagens em biblioteca

--- src/console.hpp
+++ src/console.hpp
@@ -6,6 +6,7 @@
 #include "../lib/trie.hpp"
 
 // Logic libraries
+#include "./messages.hpp"
 #include "./player.hpp"
 
 #ifndef console_h
@@ -189,10 +190,7 @@ void Console::playerCommand(string command)
 
   if (regex_match(playerArgument.begin(), playerArgument.end(), emptyString))
   {
-    cout << endl
-         << "The arguments provided are not valid" << endl
-         << endl;
-
+    invalidArgumentsMessage();
     return;
   }
 
@@ -200,10 +198,7 @@ void Console::playerCommand(string command)
 
   if (!matchedPlayers.size())
   {
-    cout << endl
-         << "No players found with given parameter" << endl
-         << endl;
-
+    noDataFoundMessage();
     return;
   }
 
@@ -250,46 +245,12 @@ void Console::exitCommand(string command)
 
 void Console::helpCommand(string command)
 {
-  cout << endl
-       << "  Commands documentation" << endl
-       << endl;
-
-  cout << "    player <name or prefix>" << endl;
-  cout << "\t- Returns the list of players whose names begins with or is equal to the parameter provided" << endl;
-  cout << "\t- Example: player Cris" << endl;
-  cout << endl;
-
-  cout << "    user <userID>" << endl;
-  cout << "\t- Returns the list of players rated by the user (20 max.), ordered from highest to lowest rating" << endl;
-  cout << "\t- Example: user 51" << endl;
-  cout << endl;
-
-  cout << "    top<N> '<position>'" << endl;
-  cout << "\t- Returns the list of the top N players in a given position, ordered from highest to lowest rating" << endl;
-  cout << "\t- Only considers players with at least 1000 ratings" << endl;
-  cout << "\t- Example: top50 'ST'" << endl;
-  cout << endl;
-
-  cout << "    tags <list of tags>" << endl;
-  cout << "\t- Returns the list of players associated with a set of tags" << endl;
-  cout << "\t- Example: tags 'Brazil' 'Dribble'" << endl;
-  cout << endl;
-
-  cout << "    help" << endl;
-  cout << "\t- List commands and documentation" << endl;
-  cout << endl;
-
-  cout << "    exit" << endl;
-  cout << "\t- Quit the console" << endl;
-  cout << endl;
+  helpMessage();
 }
 
 void Console::commandNotFound()
 {
-  cout << endl
-       << "Command not found" << endl
-       << "Use the 'help' command for more information" << endl
-       << endl;
+  commandNotFoundMessage();
 }
 
 #endif
---
+++ src/messages.hpp
@@ -0,0 +1,68 @@
+// Standard libraries
+#include <bits/stdc++.h>
+#include <iostream>
+
+#ifndef messages_h
+#define messages_h
+
+using namespace std;
+
+void helpMessage()
+{
+  cout << endl
+       << "  Commands documentation" << endl
+       << endl;
+
+  cout << "    player <name or prefix>" << endl;
+  cout << "\t- Returns the list of players whose names begins with or is equal to the parameter provided" << endl;
+  cout << "\t- Example: player Cris" << endl;
+  cout << endl;
+
+  cout << "    user <userID>" << endl;
+  cout << "\t- Returns the list of players rated by the user (20 max.), ordered from highest to lowest rating" << endl;
+  cout << "\t- Example: user 51" << endl;
+  cout << endl;
+
+  cout << "    top<N> '<position>'" << endl;
+  cout << "\t- Returns the list of the top N players in a given position, ordered from highest to lowest rating" << endl;
+  cout << "\t- Only considers players with at least 1000 ratings" << endl;
+  cout << "\t- Example: top50 'ST'" << endl;
+  cout << endl;
+
+  cout << "    tags <list of tags>" << endl;
+  cout << "\t- Returns the list of players associated with a set of tags" << endl;
+  cout << "\t- Example: tags 'Brazil' 'Dribble'" << endl;
+  cout << endl;
+
+  cout << "    help" << endl;
+  cout << "\t- List commands and documentation" << endl;
+  cout << endl;
+
+  cout << "    exit" << endl;
+  cout << "\t- Quit the console" << endl;
+  cout << endl;
+}
+
+void noDataFoundMessage()
+{
+  cout << endl
+       << "No players found with given parameter" << endl
+       << endl;
+}
+
+void invalidArgumentsMessage()
+{
+  cout << endl
+       << "The arguments provided are invalid" << endl
+       << endl;
+}
+
+void commandNotFoundMessage()
+{
+  cout << endl
+       << "Command not found" << endl
+       << "Use the 'help' command for more information" << endl
+       << endl;
+}
+
+#endif
